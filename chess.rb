284adffce3b4d41c298d70968207e47730bc80e0
Beutel <cabiv20@gmail.com>
2016-01-22 10:39:28 -0800

    Templated the Chess class

---
+++ chess.rb
@@ -0,0 +1,23 @@
+class Chess
+
+	def initialize
+		#Add logic to create new game or load an existing one
+		pass
+	end
+
+	def load_game
+		pass
+	end
+
+	def save_game
+		pass
+	end
+
+	def game_loop
+		pass
+	end
+
+	def query_player
+		pass
+	end
+end
