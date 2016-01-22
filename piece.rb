84e31d3c6722326608d0d41c95a762df93a9558f
Beutel <cabiv20@gmail.com>
2016-01-22 11:15:53 -0800

    Piece class can check if a move is legal but leaves the generation of legal moves to child classes

--- piece.rb
+++ piece.rb
@@ -2,11 +2,23 @@ class Piece
 	@@pieces = 0
 	attr_accessor :id
 	attr_accessor :color
+	attr_accessor :moves
 
 	def initialize(color)
 		@id = @@pieces
 		@color = color
 
 		@@pieces += 1
+	end
+
+	def generate_moves
+		#generates immediate possible moves
+		raise NotImplementedError.new "generate_moves"
+	end
+
+	def legal_move?(destination)
+		#checks to see if desired move is in possible moves
+		@moves.each { |coordinates| return true if (coordinates[0] == destination[0]) and (coordinates[1] == destination[1])}
+		return false
 	end
 end
