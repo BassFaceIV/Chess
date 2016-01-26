6b0b5b8ba66cfce9ff55711cd5532a60e03da07c
Beutel <cabiv20@gmail.com>
2016-01-26 10:21:05 -0800

    Board can move pieces

--- board.rb
+++ board.rb
@@ -11,12 +11,15 @@ class Board
 		#king: 4,0/7
 		@nodes[4][0] << King.new(:white)
 		@nodes[4][7] << King.new(:black)
-		
+
 		#queen: 3,0/7
 		@nodes[3][0] << Queen.new(:white)
 		@nodes[3][7] << Queen.new(:black)
 	end
 
-	#def move(piece, destination)
-	#end
+	def move(piece, destination)
+		if piece.legal_move?(destination)
+			@nodes[destination[0]][destination[1]][0] << @nodes[piece.position[0]][piece.position[1]].pop
+		end
+	end
 end
