0a96ab63495db3ff0758e2ccf1f1b9332202cfaf
Beutel <cabiv20@gmail.com>
2016-01-27 12:20:12 -0800

    King can be put in check

--- chess.rb
+++ chess.rb
@@ -28,10 +28,26 @@ end
 game = Chess.new
 game.game_loop
 game.board.move(game.board.nodes[3][0][0], [3, 6])
+game.board.pass_others(game.board.nodes[4][7][0])
+puts "Check: #{game.board.nodes[4][7][0].in_check?}"
 game.board.display
 game.board.move(game.board.nodes[1][0][0], [3, 1])
+game.board.pass_others(game.board.nodes[4][7][0])
+puts "Check: #{game.board.nodes[4][7][0].in_check?}"
 game.board.display
 game.board.move(game.board.nodes[1][0][0], [2, 2])
+game.board.pass_others(game.board.nodes[4][7][0])
+puts "Check: #{game.board.nodes[4][7][0].in_check?}"
 game.board.display
 game.board.move(game.board.nodes[2][2][0], [3, 0])
+game.board.pass_others(game.board.nodes[4][7][0])
+puts "Check: #{game.board.nodes[4][7][0].in_check?}"
+game.board.display
+game.board.move(game.board.nodes[2][2][0], [3, 4])
+game.board.pass_others(game.board.nodes[4][7][0])
+puts "Check: #{game.board.nodes[4][7][0].in_check?}"
+game.board.display
+game.board.move(game.board.nodes[3][4][0], [5, 5])
+game.board.pass_others(game.board.nodes[4][7][0])
+puts "Check: #{game.board.nodes[4][7][0].in_check?}"
 game.board.display
--- king.rb
+++ king.rb
@@ -24,80 +24,43 @@ class King < Piece
 
 		apply_boundaries(modifiers)
 
-		#modifiers.each do |modifier|
-			#newX = @position[0] + modifier[1][0]
-			#newY = @position[1] + modifier[1][1]
-			#if !in_check?(board, [newX, newY])
-			#	@moves << [newX, newY]
-			#end
-		#end
-
 		if @moves.empty?
 			@check_mate = true
 		end
 	end
 
-	def remove_move(coordinates)
-		@moves.delete([coordinates[0], coordinates[1]])
-	end
-
-	def in_check?(board, coordinates = @position)
-		queen_moves = {:n => [], :ne => [], :e => [], :se => [], :s => [], :sw => [], :w => [], :nw => []}
-		move_range =* (-7..7)
+	def in_check?
+		basic_moves = []
+		knight_moves = [[1, 2], [2, 1], [2, -1], [1, -2], [-1, -2], [-2, -1], [-2, 1], [-1, 2]]
+		move_range =* (1..7)
 
 		move_range.each do |x|
-			queen_moves[:n] << [0, x]
-			queen_moves[:ne] << [x, x]
-			queen_moves[:e] << [x, 0]
-			queen_moves[:se] << [x, -x]
-			queen_moves[:s] << [0, -x]
-			queen_moves[:sw] << [-x, -x]
-			queen_moves[:w] << [-x,0]
-			queen_moves[:nw] << [-x, x]
+			basic_moves << [0, x]
+			basic_moves << [x, x]
+			basic_moves << [x, 0]
+			basic_moves << [x, -x]
+			basic_moves << [0, -x]
+			basic_moves << [-x, -x]
+			basic_moves << [-x, 0]
+			basic_moves << [-x, x]
 		end
 
-		knight_moves = {:a => [1, 2],
-						:b => [2, 1],
-						:c => [2, -1],
-						:d => [1, -2],
-						:f => [-1, -2],
-						:g => [-2, -1],
-						:h => [-2, 1],
-						:i => [-1, 2]}
+		apply_boundaries(basic_moves)
+		basic_moves += knight_moves
+		apply_knight_friendlies(basic_moves)
+		basic_moves.delete_if { |mod| (@position[0] + mod[0] < 0) || (@position[0] + mod[0] > 7) || (@position[1] + mod[1] < 0) || (@position[1] + mod[1] > 7) }
 
-		bishop_moves = apply_boundaries(coordinates, bishop_moves)
-
-		queen_moves.each do |move|
-			move[1].each do |distance|
-				location = board.nodes[coordinates[0] + distance[0]][coordinates[1] + distance[1]][0]
-				if (!location.nil?) && (location.color != @color)
-					if location.is_a?(Bishop) || location.is_a?(Queen) || location.is_a?(Rook)
-						return true
-					end
-
-					if ((distance[0].abs == 1) || (distance[1].abs == 1)) && location.is_a?(King)
-						return true
-					end
-
-					if location.is_a?(Pawn) && (distance[0].abs == 1)
-						if (@color == :black) && (distance[1] == -1)
-							return true
-						end
-
-						if (@color == :white) && (distance[1] == 1)
-							return true
-						end
-					end
-				end
+		return basic_moves.any? do |move|
+			@enemies.any? do |enemy|
+				enemy == [@position[0] + move[0], @position[1] + move[1]]
 			end
 		end
+	end
 
-		knight_moves = apply_boundaries(coordinates, knight_moves)
-
-		knight_moves.each do |move|
-			location = board.nodes[coordinates[0] + move[1][0]][coordinates[1] + move[1][1]][0]
-			if (!location.nil?) && (location.color != @color) && location.is_a?(Knight)
-				return true
+	def apply_knight_friendlies(modifiers)
+		modifiers.delete_if do |mod|
+			@allies.any? do |ally|
+				ally == [@position[0] + mod[0], @position[1] + mod[1]]
 			end
 		end
 	end
